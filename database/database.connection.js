bca5ed4d2791a8215bed67b8eb501ed43ef1c742
m4arthu <luisarthir259@gmail.com>
2023-08-05 14:59:47 -0300

    fianlizado

--- database/database.connection.js
+++ database/database.connection.js
@@ -3,8 +3,18 @@ import pg from  "pg"
 dotenv.config()
 const {Pool} = pg
 
-const  configDatabase =  {
-    connectionString: process.env.DATABASE_URL,
+let configDatabase
+if(process.env.NODE_ENV === "producion"){
+     configDatabase =  {
+        connectionString: process.env.DATABASE_URL,
+        ssl:true
+    }
+} else {
+    configDatabase =  {
+        connectionString: process.env.DATABASE_URL,
+       
+    }
 }
 
+console.log(configDatabase)
 export const db  = new Pool(configDatabase)
